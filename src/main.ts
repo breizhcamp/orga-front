6e3b1d982e181bca7de387febc564dc0ba27f83c
claire-lucas <claire.lucas.perso@gmail.com>
2024-07-03 09:39:22 +0200

    use KeyCloak.js instead of VueCloak
    Also upgrades bootstrap icons to 1.11

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-// @ts-ignore
-import { Vuecloak } from 'vuecloak'
+import KeyCloak from 'keycloak-js'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './assets/main.css'
@@ -17,16 +16,48 @@ axios.interceptors.request.use(async config => {
   config.headers.Authorization = `Bearer ${app.config.globalProperties.$keycloak.token}`
   return config
 })
-app.use(Vuecloak, {
-  init: {
-    onLoad: 'login-required',
-    checkLoginIframe: false
-  },
-  config: {
-    url: 'https://auth.breizhcamp.org/auth/',
-    realm: 'BreizhCamp',
-    clientId: 'bilhed-admin-front',
-  }
+
+const vueCloak = new KeyCloak({
+  url: 'https://auth.breizhcamp.org/auth/',
+  realm: 'BreizhCamp',
+  clientId: 'bilhed-admin-front'
 })
-app.use(router)
-app.mount('#app')
+
+const getVueRoute = () => {
+  // get URL
+  const location = window.location.href;
+  // Return '/' without hash  
+  if (!location.includes('#')) return '/';
+
+  // Get URL string after hash 
+  const start = location.indexOf('#');
+  const end = location.length;
+  let vueRoute = location.substring(start + 1, end);
+  // Check if all keycloak params exist
+  // And custom route query should avoid keycloak query
+  const keycloakParams = [
+    vueRoute.includes('&code'),
+    vueRoute.includes('&state'),
+    vueRoute.includes('&session_state')
+  ];
+  const state = keycloakParams.every((item) => item);
+  // if state=true, replace URL
+  if (state) {
+    vueRoute = vueRoute
+      .replace(/[&?]code=[^&$]*/, "")
+      .replace(/[&?]state=[^&$]*/, "")
+      .replace(/[&?]session_state=[^&$]*/, "");
+  }
+  return vueRoute;
+}
+
+vueCloak.init({
+  onLoad: 'login-required',
+  checkLoginIframe: false
+}).then(async () => {
+  app.config.globalProperties.$keycloak = vueCloak;
+  app.use(router)
+  app.mount('#app');
+  const vueRoute = getVueRoute();
+  await router.push(vueRoute)
+})
