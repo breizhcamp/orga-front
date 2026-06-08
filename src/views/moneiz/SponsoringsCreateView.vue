34fb67f148c9a26f35a853f75a1ae55832f973a7
Floris Bartra <floris.bartra@gmail.com>
2026-06-08 16:55:03 +0200

    Fix create sponsoring form

--- src/views/moneiz/SponsoringsCreateView.vue
+++ src/views/moneiz/SponsoringsCreateView.vue
@@ -98,6 +98,7 @@ const handleSubmit = async () => {
             <input
               id="number-slots"
               placeholder="Nombre de slots"
+              class="form-control"
               :min="1"
               :step="1"
               v-model="numberSlots"
